cf7deb10772eb94f4c419f35e43b75afd94bdcef
Sheeseburger <kekymber@gmail.com>
2024-10-07 15:14:39 +0300

    change to multi server role

--- index.js
+++ index.js
@@ -3,32 +3,29 @@ require('./utils/morning');
 
 const amqp = require('amqplib');
 const express = require('express');
+const {createEventAdapter} = require('@slack/events-api');
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 const sendTelegramNotification = require('./bot-entity/telegram');
 const {sendEmail} = require('./bot-entity/gmail');
 const {
   sendDirectMessage,
   sendGroupMessage,
-  sendConfirmationMessage,
-  slackApp
+  sendConfirmationMessage
 } = require('./bot-entity/slack');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const queue_name = process.env.QUEUE_NAME;
 
-// Назви черг для кожного типу повідомлень
-const tgQueueName = 'tg_queue';
-const emailQueueName = 'email_queue';
-const slackQueueName = 'slack_queue';
-const message = 'message_queue';
 const processTelegramMessage = async body => {
   const {chatId, text} = body;
-  await sendTelegramNotification(chatId, text);
+  if (chatId && text && text.length > 0) await sendTelegramNotification(chatId, text);
 };
 
 const processEmailMessage = async body => {
   const {email, subject, message, html, sender} = body;
-  if ((email, message))
+  if (email && message)
     await sendEmail({
       sender: sender,
       email: email,
@@ -43,10 +40,11 @@ const processSlackMessage = async body => {
 
   if (body.type === 'slack_direct') {
     const {userName, userId, text} = body.body;
-    await sendDirectMessage(userName, userId, text);
+    if (userName && text && userName.length > 0 && text.length > 0)
+      await sendDirectMessage(userName, userId, text);
   } else if (body.type === 'slack_group') {
     const {channelId, text} = body.body;
-    await sendGroupMessage(channelId, text);
+    if (channelId && text) await sendGroupMessage(channelId, text);
   } else if (body.type === 'slack_group_confirm_subgroup') {
     const {text, subgroupId, userId, channelId} = body.body;
     await sendConfirmationMessage(channelId, subgroupId, userId, text);
@@ -60,45 +58,25 @@ const start = async () => {
     const connection = await amqp.connect(process.env.RABBITMQ_URL);
     const channel = await connection.createChannel();
 
-    // Оголошення черг
-    await channel.assertQueue(tgQueueName, {durable: true});
-    await channel.assertQueue(emailQueueName, {durable: true});
-    await channel.assertQueue(message, {durable: true});
-    await channel.assertQueue(slackQueueName, {durable: true});
+    // Ожидание сообщений из указанной очереди
+    await channel.assertQueue(queue_name, {durable: true});
 
-    console.log(
-      `Waiting for messages in queues: ${tgQueueName}, ${emailQueueName}, ${slackQueueName}`
-    );
+    console.log(`Waiting for messages in queue: ${queue_name}`);
 
-    channel.consume(tgQueueName, async msg => {
+    channel.consume(queue_name, async msg => {
       if (msg !== null) {
         const messageContent = JSON.parse(msg.content.toString());
-        await processTelegramMessage(messageContent);
-        channel.ack(msg);
-      }
-    });
 
-    channel.consume(message, async msg => {
-      if (msg !== null) {
-        const messageContent = JSON.parse(msg.content.toString());
-        // await processEmailMessage(messageContent);
-        console.log(messageContent);
-        channel.ack(msg);
-      }
-    });
-    channel.consume(emailQueueName, async msg => {
-      if (msg !== null) {
-        const messageContent = JSON.parse(msg.content.toString());
-        await processEmailMessage(messageContent);
-        channel.ack(msg);
-      }
-    });
+        if (queue_name === 'tg_queue') {
+          await processTelegramMessage(messageContent);
+        } else if (queue_name === 'email_queue') {
+          await processEmailMessage(messageContent);
+        } else if (queue_name === 'slack_queue') {
+          await processSlackMessage(messageContent);
+        } else {
+          console.log('Unknown queue:', queue_name);
+        }
 
-    channel.consume(slackQueueName, async msg => {
-      if (msg !== null) {
-        const messageContent = JSON.parse(msg.content.toString());
-        console.log(messageContent);
-        await processSlackMessage(messageContent);
         channel.ack(msg);
       }
     });
@@ -108,10 +86,11 @@ const start = async () => {
 };
 
 start();
-const {createEventAdapter} = require('@slack/events-api');
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
-app.use('/slack/events', slackEvents.expressMiddleware());
-slackEvents.on('error', console.error);
+
+if (queue_name === 'slack') {
+  app.use('/slack/events', slackEvents.expressMiddleware());
+  slackEvents.on('error', console.error);
+}
 
 app.get('/', (req, res) => {
   res.send('Service is running');
