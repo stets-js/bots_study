4aeeea271bde0a66c5250420a50c9bde7ac55f7e
Sheeseburger <kekymber@gmail.com>
2024-10-07 16:45:32 +0300

    updating index

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ const start = async () => {
 
 start();
 
-if (queue_name === 'slack') {
+if (queue_name === 'slack_queue') {
   app.use('/slack/events', slackEvents.expressMiddleware());
   slackEvents.on('error', console.error);
 }
@@ -95,27 +95,7 @@ if (queue_name === 'slack') {
 app.get('/', (req, res) => {
   res.send('Service is running');
 });
-start(); // Start RabbitMQ consumer
 
-if (queue_name === 'slack') {
-  // Start Slack Bolt server
-  (async () => {
-    await slackApp.start(port);
-    console.log(`⚡️ Slack Bolt app is running on port ${port}`);
-
-    slackApp.event('message', async ({event, say}) => {
-      await say(`Hello, <@${event.user}>!`);
-    });
-  })();
-} else {
-  // Start Express server for other queues
-  const app = express();
-
-  app.get('/', (req, res) => {
-    res.send('Express server is running');
-  });
-
-  app.listen(port, () => {
-    console.log(`Express server is listening on port ${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
