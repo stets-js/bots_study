6c51b055f64dfd979b3d3c7202d08c09e3f14fdb
monyshchen_notebook <kekymber@gmail.com>
2024-10-31 12:55:04 +0200

    updated slack flow

--- utils/sendShiftData.js
+++ utils/sendShiftData.js
@@ -1,22 +1,29 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function sendShiftData(body, status) {
-  const userSlackId = body.user.id;
-  const channelId = body.channel.id;
-  const date = new Date();
+    const userSlackId = body.user.id;
+    const channelId = body.channel.id;
+    const date = new Date();
 
-  try {
-    const response = await axios.post('https://dolphin-app-b3fkw.ondigitalocean.app/api/shift', {
-      userSlackId,
-      date,
-      status,
-      channelId
-    });
-
-    console.log('Дані успішно надіслані:', response.data);
-  } catch (error) {
-    console.error('Помилка при надсиланні даних:', error.response?.data || error.message);
-  }
+    try {
+        const response = await axios.post(
+            "https://dolphin-app-b3fkw.ondigitalocean.app/api/shift",
+            {
+                userSlackId,
+                date,
+                status,
+                channelId,
+            }
+        );
+        return response;
+        console.log("Дані успішно надіслані:", response.data);
+    } catch (error) {
+        return error;
+        console.error(
+            "Помилка при надсиланні даних:",
+            error.response?.data || error.message
+        );
+    }
 }
 
-module.exports = {sendShiftData};
+module.exports = { sendShiftData };
