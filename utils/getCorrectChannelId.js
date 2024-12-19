b5caf388d221111984d4f907a1a37e052c969a61
Sheeseburger <kekymber@gmail.com>
2024-12-19 12:43:29 +0200

    added sup channel id

--- utils/getCorrectChannelId.js
+++ utils/getCorrectChannelId.js
@@ -1,6 +1,13 @@
 const userInSelectedChannel = async (shiftType = '', userId, client) => {
   console.log(shiftType, userId);
-  const channelId = shiftType === 'kwiz' ? 'C07UADS7U3G' : shiftType === 'om' ? 'C07U2G5J7PH' : '';
+  const channelId =
+    shiftType === 'kwiz'
+      ? 'C07UADS7U3G'
+      : shiftType === 'om'
+      ? 'C07U2G5J7PH'
+      : shiftType === 'sup'
+      ? 'C083PKS3L0M'
+      : '';
   const channel = await client.conversations.members({channel: channelId});
 
   const isMember = channel.members.includes(userId);
