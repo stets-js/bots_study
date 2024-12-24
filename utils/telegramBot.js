4bc78118ce4d1c01273525ee6effb8cfd31abd8a
Sheeseburger <kekymber@gmail.com>
2024-12-24 15:11:23 +0200

    set false to pollinbg

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT, {polling: true});
+const bot = new TelegramBot(process.env.TELEGRAM_BOT, {polling: false});
 
 module.exports = bot;
