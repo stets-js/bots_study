f74229af3dff01bee5a3a7b496f3a1889e3aab6c
Sheeseburger <kekymber@gmail.com>
2024-12-24 14:25:17 +0200

    added tg bot to gmail, removed slack from index

--- bot-entity/telegram.js
+++ bot-entity/telegram.js
@@ -1,17 +1,78 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const queue_name = process.env.QUEUE_NAME;
-if (queue_name === 'tg_queue') {
-  const bot = new TelegramBot(process.env.TELEGRAM_BOT, {polling: true});
+const axios = require('axios');
+const bot = new TelegramBot(process.env.TELEGRAM_BOT, {polling: true});
 
-  const sendTelegramNotification = async (chatId, message, options = {}) => {
-    try {
-      await bot.sendMessage(chatId, message, options);
-      console.log(`Message sent to chat ${chatId}`);
-    } catch (error) {
-      console.error('Error sending Telegram message:', error);
+const userStates = {};
+
+bot.onText(/\/start-sync/, msg => {
+  const chatId = msg.chat.id;
+
+  userStates[chatId] = {step: 'askEmail'};
+
+  bot.sendMessage(chatId, 'Відправте свою пошту з букінга:');
+});
+
+bot.on('message', async msg => {
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+
+  if (userStates[chatId]) {
+    const userState = userStates[chatId];
+
+    if (userState.step === 'askEmail') {
+      const email = msg.text.trim();
+
+      if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        return bot.sendMessage(chatId, 'Невірний формат пошти. Спробуйте ще раз:');
+      }
+
+      userState.email = email;
+      userState.step = 'askPassword';
+      return bot.sendMessage(chatId, 'Тепер відправте пароль з букінга:');
     }
-  };
 
-  module.exports = sendTelegramNotification;
-}
+    if (userState.step === 'askPassword') {
+      const password = msg.text.trim();
+      userState.password = password;
+
+      console.log(
+        `User ${chatId} (ID: ${userId}) provided email: ${userState.email} and password: ${password}`
+      );
+
+      try {
+        const response = await axios.patch(
+          'https://dolphin-app-b3fkw.ondigitalocean.app/api/users/telegram',
+          {
+            email: userState.email,
+            chatId: chatId,
+            userId: userId,
+            password: password
+          }
+        );
+
+        if (response.status === 200) {
+          await bot.sendMessage(chatId, 'Синхронизация успешно завершена!');
+        } else {
+          await bot.sendMessage(chatId, 'Щось пішло не так, спробуйте ще раз.');
+        }
+      } catch (error) {
+        console.error('Error syncing user:', error);
+        await bot.sendMessage(chatId, 'Щось пішло не так, спробуйте ще раз.');
+      }
+
+      delete userStates[chatId];
+    }
+  }
+});
+
+const sendTelegramNotification = async (chatId, message, options = {}) => {
+  try {
+    await bot.sendMessage(chatId, message, options);
+    console.log(`Message sent to chat ${chatId}`);
+  } catch (error) {
+    console.error('Error sending Telegram message:', error);
+  }
+};
+
+module.exports = sendTelegramNotification;
--- index.js
+++ index.js
@@ -3,21 +3,12 @@ require('./utils/morning');
 
 const amqp = require('amqplib');
 const express = require('express');
-const {createEventAdapter} = require('@slack/events-api');
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 const sendTelegramNotification = require('./bot-entity/telegram');
 const {sendEmail} = require('./bot-entity/gmail');
-const {
-  sendDirectMessage,
-  sendGroupMessage,
-  sendConfirmationMessage,
-  slackApp
-} = require('./bot-entity/slack');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const queue_name = process.env.QUEUE_NAME;
 
 const processTelegramMessage = async body => {
   const {chatId, text} = body;
@@ -36,25 +27,7 @@ const processEmailMessage = async body => {
     });
 };
 
-const processSlackMessage = async body => {
-  const {type, ...rest} = body;
-  console.log(body);
-  if (body.type === 'slack_direct') {
-    const {userName, userId, text} = body.body;
-    if (userName && text && userName.length > 0 && text.length > 0)
-      await sendDirectMessage(userName, userId, text);
-  } else if (body.type === 'slack_group') {
-    const {channelId, text} = body.body;
-    if (channelId && text) await sendGroupMessage(channelId, text);
-  } else if (body.type === 'slack_group_confirm_subgroup') {
-    const {text, subgroupId, userId, channelId} = body.body;
-    await sendConfirmationMessage(channelId, subgroupId, userId, text);
-  } else {
-    console.log('Unsupported Slack message type:', type);
-  }
-};
-
-const processQueueMessages = async channel => {
+const processQueueMessages = async (channel, queue_name) => {
   try {
     const msg = await channel.get(queue_name, {noAck: false});
 
@@ -65,8 +38,6 @@ const processQueueMessages = async channel => {
         await processTelegramMessage(messageContent);
       } else if (queue_name === 'email_queue') {
         await processEmailMessage(messageContent);
-      } else if (queue_name === 'slack_queue') {
-        await processSlackMessage(messageContent);
       } else {
         console.log('Unknown queue:', queue_name);
       }
@@ -80,41 +51,26 @@ const processQueueMessages = async channel => {
   }
 };
 
-const start = async () => {
+const startQueue = async queue_name => {
   try {
     const connection = await amqp.connect(process.env.RABBITMQ_URL);
     const channel = await connection.createChannel();
 
     await channel.assertQueue(queue_name, {durable: true});
     console.log(`Waiting for messages in queue: ${queue_name}`);
-
-    setInterval(async () => {
-      await processQueueMessages(channel);
-    }, 5000); // 10000 миллисекунд = 10 секунд
+    processQueueMessages(channel, queue_name);
   } catch (error) {
     console.error('Error in RabbitMQ service:', error);
   }
 };
 
-start();
+startQueue('email_queue');
+startQueue('tg_queue');
 
-if (queue_name === 'slack_queue') {
-  (async () => {
-    await slackApp.start(port);
-    console.log(`⚡️ Slack Bolt app is running on port ${port}`);
+app.get('/', (req, res) => {
+  res.send('Express server is running');
+});
 
-    slackApp.event('message', async ({event, say}) => {
-      await say(`Hello, <@${event.user}>!`);
-    });
-  })();
-} else {
-  const app = express();
-
-  app.get('/', (req, res) => {
-    res.send('Express server is running');
-  });
-
-  app.listen(port, () => {
-    console.log(`Express server is listening on port ${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Express server is listening on port ${port}`);
+});
