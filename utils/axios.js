d42688c31a14e67116021036e78998be71bd9df7
Sheeseburger <kekymber@gmail.com>
2025-02-17 11:57:07 +0200

    fix: remove debug console log in sendStatusUpdate for cleaner code

--- utils/axios.js
+++ utils/axios.js
@@ -4,6 +4,7 @@ axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.baseURL = 'https://dolphin-app-b3fkw.ondigitalocean.app/api';
 
 const sendStatusUpdate = async (token, data) => {
+  console.log(data, 'data!!');
   const url = `https://dolphin-app-b3fkw.ondigitalocean.app/api/subgroups/${data.subgroupId}/status?action=${data.status}&mentorId=${data.mentorId}`;
 
   return axios.post(url, data, {
