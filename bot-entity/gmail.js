9888975f3206932001b833dddda627427e8dd51b
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2025-04-16 14:06:04 +0300

    Update gmail.js

--- bot-entity/gmail.js
+++ bot-entity/gmail.js
@@ -1,11 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  // host: process.env.EMAIL_HOST,
-  service: process.env.EMAIL_SERVICE,
-  // port: 587,
-  // port: 465,
-  // secure: true,
+  host: process.env.EMAIL_HOST,
+  // service: process.env.EMAIL_SERVICE,
+  port: 2525,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
